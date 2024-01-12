e71066f8e3bacd35624866d66caab460a62c6eda
Ray <raypeaslee@gmail.com>
2024-01-12 07:41:12 -0500

    add server comments

--- index.mjs
+++ index.mjs
@@ -16,20 +16,28 @@ app.use('/contact', contactRoutes);
 const key = readFileSync(process.env.OPP_KEY)
 const cert = readFileSync(process.env.OPP_CERT)
 
-// Create the server
-const server = createServer({
-  key: key,
-  cert: cert  
-}, app)
+/**
+ * Create an HTTPS server using the key, cert and app.
+ * Listen on the port from OPP_PORT env var.
+ * Log any errors starting the server.
+ * Log a message when the server starts listening.
+ */
+const server = createServer(
+  {
+    key: key,
+    cert: cert,
+  },
+  app
+);
 
-server.on('error', err => {
-  console.error('Error starting server:', err)
-})
+server.on("error", (err) => {
+  console.error("Error starting server:", err)
+});
 
-server.listen(process.env.OPP_PORT, err => {
+server.listen(process.env.OPP_PORT, (err) => {
   if (err) {
-    console.error('Error starting server:', err)
+    console.error("Error starting server:", err)
   } else {
-    console.log('Server listening on port:', process.env.OPP_PORT) 
+    console.log("Server listening on port:", process.env.OPP_PORT)
   }
 })
