2350808aeb24ba5878bd2e61553f46c91a35b321
Ray <raypeaslee@gmail.com>
2024-01-11 21:03:18 -0500

    routes are working

--- index.mjs
+++ index.mjs
@@ -4,24 +4,22 @@ import { readFileSync } from 'node:fs'
 import express from 'express'
 
 const app = express()
-const options = {
-  key: readFileSync(process.env.OPP_KEY),
-  cert: readFileSync(process.env.OPP_CERT)
-};
 
-try {
-  createServer(options, (req, res) => {
-    try {
-      res.writeHead(200);
-      res.end('homepage');
-    } catch (err) {
-      console.error(err);
-      res.statusCode = 500;
-      res.end('Internal Server Error'); 
-    }
-  }, app).listen(process.env.OPP_PORT);
-} catch (err) {
-  console.error('Error starting HTTPS server:', err);
-  process.exit(1);
-}
+// Require the route files
+import homeRoutes from './routes/home.mjs'
+import contactRoutes from './routes/contact.mjs'
+
+// Mount the routes 
+app.use('/', homeRoutes);
+app.use('/contact', contactRoutes);
+
+const key = readFileSync(process.env.OPP_KEY)
+const cert = readFileSync(process.env.OPP_CERT)
+
+// Start the server
+const server = createServer({
+  key: key,
+  cert: cert
+}, app).listen(process.env.OPP_PORT)
+
 console.log(`Server listening on port: ${process.env.OPP_PORT}`);
